8d320f00293c528cf2d6b2ebfc88a3dde09b45b4
PstErti <340470024@qq.com>
2025-12-07 00:12:53 +0800

    Updated license info in source code files.

--- src/wheel.h
+++ src/wheel.h
@@ -1,3 +1,34 @@
+/**
+ *
+ * @file: wheel.h
+ *
+ * @author: PstErti (https://github.com/PstErti)
+ *
+ * @date: 2025/12/07
+ *
+ * @license:
+ * The MIT License (MIT)
+ * Copyright © 2025 @PstErti
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 #include <cstdint>
 
 alignas(64) const uint16_t WHEEL_30_W[] = {1, 7, 11, 13, 17, 19, 23, 29};
